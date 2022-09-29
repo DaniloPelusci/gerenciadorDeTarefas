02f3861a3137582c0e8efe139c0a3f38ff040c8b
Danilo <danilopelusci@gmail.com>
2022-09-29 00:25:48 -0300

    implementando a listagem de tarefas

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { TarefaService } from './shared';
-import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
+import { ListarTarefaComponent } from './listar';
 
 
 @NgModule({
@@ -10,7 +12,9 @@ import { ListarTarefaComponent } from './listar-tarefa/listar-tarefa.component';
     ListarTarefaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
